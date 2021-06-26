789a94ce8f1312a1383f33b3cc30e1033908de5e
todokete <fabio-marras@hotmail.com>
2021-06-26 18:05:47 -0300

    Navbar ready.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <div className="container">
                 <a className="navbar-brand" href="/#"><h1>React Portfolio</h1></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <i class="fas fa-grip-lines"></i>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
