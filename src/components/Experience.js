49495ad9c5cc5ac2f3070c4cc9582aeb11c9113c
todoketee <fabio-marras@hotmail.com>
2025-03-14 08:38:53 +0100

    reverting the change

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -47,7 +47,7 @@ const Experience = () => {
             <div className='timeline-content'>
               <h3>2022</h3>
               <p>
-              It was the year I graduated. test 
+              It was the year I graduated.
               </p>
             </div>
           </div>
